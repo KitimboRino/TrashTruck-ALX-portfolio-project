d8647661570dfc830e5518e5ad8ad878a882f83a
peterlubega <peterlubega100@gmail.com>
2024-05-19 11:12:15 +0000

    feat: added init, models, config and run then adjusted app.py

---
+++ models.py
@@ -0,0 +1,20 @@
+from app import db
+from sqlalchemy.orm import mapped_column
+from sqlalchemy import String, Integer
+
+class User(db.Model):
+    id = mapped_column(Integer, primary_key=True)
+    username = mapped_column(String(64), index=True, unique=True)
+    email = mapped_column(String(120), index=True, unique=True)
+    password_hash = mapped_column(String(256))
+
+    def set_password(self, password):
+        from werkzeug.security import generate_password_hash
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        from werkzeug.security import check_password_hash
+        return check_password_hash(self.password_hash, password)
+
+    def __repr__(self):
+        return '<User {}>'.format(self.username)
